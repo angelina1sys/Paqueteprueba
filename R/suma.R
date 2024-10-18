4c46bc9ee530510aae8d05cf6aba37cdefd79788
Angelina Luciani <angelinaluciani2016@gmail.com>
2024-10-18 11:16:32 -0300

    agrego chequeo automático

--- R/suma.R
+++ R/suma.R
@@ -1,31 +1,32 @@
-
 #' Suma dos numeros
 #'
-#' Esta función toma 2 numeros positivos y los suma.
-#' 1
-#' @param x es un vetor numerico
-#' @param y es un vector numerico
+#' La función `suma()` toma 2 números positivos y los suma.
+#'
+#' Estos son detalles extras de la función.
+#'
+#' @param x un vector numrico positivo
+#' @param y un vector numrico
 #'
 #' @return
-#' Un vector numerico con la suma de `x` e `y`
-#'
-#' @export
+#' Un vector numerico con la suma de `x` e `y`.
 #'
 #' @examples
 #' suma(2, 2)
 #' suma(1, 10)
 #'
+#'
+#' @export
 suma <- function(x = 2, y = 2) {
 
+  # Chequea que los argumentos sean num\U00E9ricos
   if (!is.numeric(x) | !is.numeric(y)) {
 
     cli::cli_abort(c(
-      "i" = "Los argumentos deben ser numericos.",
+      "i" = "Los argumentos deben ser num\U00E9ricos.",
       "x" = "x es {class(x)}, y es {class(y)}"
     ))
   }
 
-  #chequea que los argumentos sean numericos
   if (sign(x) < 0 | sign(y) < 0) {
     cli::cli_abort(c(
       "i" = "No puedo sumar negativos."
